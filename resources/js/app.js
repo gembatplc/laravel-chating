a79553a2a872ddc0009a728684fbb1529b2d41cd
gembatplc <gembatplc@github.com>
2021-04-12 12:08:17 +0600

    vue ui kit install

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,28 @@ require('./bootstrap');
 
 window.Vue = require('vue').default;
 
+
+import { CometChat } from "@cometchat-pro/chat/CometChat";
+
+
+let cometChatAppSetting = new CometChat.AppSettingsBuilder()
+    .subscribePresenceForAllUsers()
+    .setRegion(process.env.MIX_COMMETCHAT_APP_REGION)
+    .build();
+
+CometChat.init(process.env.MIX_COMMETCHAT_APP_ID, cometChatAppSetting).then(
+    () => {
+        console.log("Initialization completed successfully");
+
+        const app = new Vue({
+            el: '#app',
+        });
+    },
+    (error) => {
+        console.log("Initialization failed with error:", error);
+    }
+);
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
